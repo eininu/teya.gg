a31dc9a29ab4abdaf4b61456d9ba5a0b876b22e3
ein <ein@jp9.org>
2023-12-24 15:18:26 +0100

    Connect UI from tailkit.com

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,5 +2,5 @@ import { Routes } from '@angular/router';
 import { WebsitesComponent } from './websites/websites.component';
 export const routes: Routes = [
   { path: 'websites', component: WebsitesComponent },
-  { path: '', redirectTo: '/websites', pathMatch: 'full' },
+  // { path: '', redirectTo: '/websites', pathMatch: 'full' },
 ];
