00ada911900ea2dfe728e62ec98b6c5cac3563fc
ein <ein@jp9.org>
2023-12-24 16:01:23 +0100

    Setup basic app UI template

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,14 +1,31 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, RouterModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'frontend';
+  constructor(private router: Router) {}
+
+  title = 'Teya.gg';
+  isActive(route: string): boolean {
+    return this.router.url === route;
+  }
+
+  getClass(route: string): string {
+    const active = this.isActive(route);
+    return `
+          ${
+            active
+              ? 'group flex items-center space-x-2 rounded-lg border border-blue-100 bg-blue-50 px-3 py-2 text-sm font-medium text-blue-600 dark:border-transparent dark:bg-gray-700 dark:text-white'
+              : 'group flex items-center space-x-2 rounded-lg border border-transparent px-3 py-2 text-sm font-medium text-gray-800 hover:bg-blue-50 hover:text-blue-600 active:border-blue-100 dark:text-gray-200 dark:hover:bg-gray-700 dark:hover:text-white dark:active:border-gray-600'
+          }
+          dark:text-gray-200`;
+  }
 }
