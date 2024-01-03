0e7ec92b36d1f462809105df9327c1968384c81f
ein <ein@jp9.org>
2024-01-03 19:52:04 +0100

    Setup basic app UI template

---
+++ frontend/src/Home/Home.js
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <>
+      {/* Placeholder */}
+      <div className="flex items-center justify-center rounded-xl border-2 border-dashed border-gray-200 bg-gray-50 py-64 text-gray-400 dark:border-gray-700 dark:bg-gray-800">
+        Content (max width 1280px)
+      </div>
+    </>
+  );
+}
