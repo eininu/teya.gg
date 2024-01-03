964884ea31c4a242c73bc7c68efa84de83d4d259
ein <ein@jp9.org>
2024-01-03 21:22:16 +0100

    Read websites list from api

--- frontend/src/Websites/Websites.js
+++ frontend/src/Websites/Websites.js
@@ -1,3 +1,26 @@
+import React, { useEffect, useState } from "react";
+
 export default function Websites() {
-  return "Websites";
+  const [websites, setWebsites] = useState([]);
+
+  useEffect(() => {
+    fetch("/api/websites")
+      .then((response) => response.json())
+      .then((data) => setWebsites(data))
+      .catch((error) => console.error("Error while receiving data:", error));
+  }, []);
+
+  return (
+    <div>
+      {websites.map((website) => (
+        <div key={website.id}>
+          {website.isDomainRoskomnadzorBanned ? (
+            <s>{website.domainName}</s>
+          ) : (
+            <span>{website.domainName}</span>
+          )}
+        </div>
+      ))}
+    </div>
+  );
 }
