951f263c4f1c6e2e3db312202db77a04afb99882
ein <ein@jp9.org>
2024-01-16 16:26:54 +0100

    Prepare pbn boilerplate

--- pbn/_data/links.js
+++ pbn/_data/links.js
@@ -1,16 +1,4 @@
-const getLinks = async () => {
-  try {
-    const fetch = (...args) =>
-      import("node-fetch").then(({ default: fetch }) => fetch(...args));
-    const response = await fetch("http://localhost/api/pbn-links");
-    if (!response.ok) {
-      throw new Error("Network response was not ok");
-    }
-    return response.json();
-  } catch (error) {
-    console.error("Fetch error:", error);
-    return {};
-  }
+module.exports = {
+  "example1.com": ["test11.com", "test22.com"],
+  "example2.com": ["test33.com", "test44.com"],
 };
-
-module.exports = getLinks();
