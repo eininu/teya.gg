de538119c208fa2da36b8dec56e8c9fae00d003c
ein <ein@jp9.org>
2024-01-19 07:59:27 +0100

    Show Websites component in homepage

--- frontend/src/Home/Home.js
+++ frontend/src/Home/Home.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import Websites from "../Websites/Websites";
 
 export default function Home() {
   const [websitesLength, setWebsitesLength] = useState(0);
@@ -17,10 +18,15 @@ export default function Home() {
   return (
     <>
       {/* Placeholder */}
-      <div className="flex items-center justify-center rounded-xl border-2 border-dashed border-gray-200 bg-gray-50 py-64 text-gray-400 dark:border-gray-700 dark:bg-gray-800">
-        You have <span className={"text-green-500 m-1"}>{websitesLength}</span>{" "}
+      {/*<div className="flex items-center justify-center rounded-xl border-2 border-dashed border-gray-200 bg-gray-50 py-64 text-gray-400 dark:border-gray-700 dark:bg-gray-800">*/}
+      {/*  You have <span className={"text-green-500 m-1"}>{websitesLength}</span>{" "}*/}
+      {/*  websites!*/}
+      {/*</div>*/}
+      <p className={"pl-4 text-xs"}>
+        You have <span className={"text-green-500"}>{websitesLength}</span>{" "}
         websites!
-      </div>
+      </p>
+      <Websites />
     </>
   );
 }
