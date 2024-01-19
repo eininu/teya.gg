6ad3a9bf31a8e416ce549fbe7b2ffd9c68b2a150
ein <ein@jp9.org>
2024-01-19 07:24:09 +0100

    Show count websites

--- frontend/src/Home/Home.js
+++ frontend/src/Home/Home.js
@@ -1,9 +1,25 @@
+import { useEffect, useState } from "react";
+
 export default function Home() {
+  const [websitesLength, setWebsitesLength] = useState(0);
+
+  const fetchWebsites = async () => {
+    const response = await fetch("/api/websites");
+    const data = await response.json();
+    console.log(data);
+    setWebsitesLength(data.length);
+  };
+
+  useEffect(() => {
+    fetchWebsites();
+  }, []);
+
   return (
     <>
       {/* Placeholder */}
       <div className="flex items-center justify-center rounded-xl border-2 border-dashed border-gray-200 bg-gray-50 py-64 text-gray-400 dark:border-gray-700 dark:bg-gray-800">
-        Content (max width 1280px)
+        You have <span className={"text-green-500 m-1"}>{websitesLength}</span>{" "}
+        websites!
       </div>
     </>
   );
