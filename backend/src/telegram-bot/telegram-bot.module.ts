834f8d2a433d1d437942369a0e7cdf3b75cbcc2d
ein <ein@jp9.org>
2024-03-08 15:48:54 +0100

    Fix problem with telegraf module

--- backend/src/telegram-bot/telegram-bot.module.ts
+++ backend/src/telegram-bot/telegram-bot.module.ts
@@ -1,15 +1,13 @@
-import {Global, Module} from "@nestjs/common";
-import { TelegrafModule } from "nestjs-telegraf";
-import { TelegramService } from "./telegram-bot.service";
+import { Global, Module } from '@nestjs/common';
+import { TelegrafModule } from 'nestjs-telegraf';
+import { TelegramService } from './telegram-bot.service';
 
-const env = process.env
+const env = process.env;
 
 @Global()
 @Module({
-    imports: [TelegrafModule.forRoot({
-        token: env.TELEGRAM_BOT_TOKEN || '',
-    })],
-    providers: [TelegramService],
-    exports: [TelegramService],
+  imports: [TelegrafModule],
+  providers: [TelegramService],
+  exports: [TelegramService],
 })
 export class TelegramModule {}
